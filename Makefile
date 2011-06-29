0078baafa805d109ccf8f7b06be85af8bc33f377
MaskRay <emacsray@gmail.com>
2011-06-29 09:54:15 +0800

    naive and fast search

--- Makefile
+++ Makefile
@@ -1,7 +1,31 @@
-CXXFLAGS = -g
-main: main.cpp
-a.out: main.cpp
-	$(CXX) $(CXXFLAGS) -D LINEAR_BUILD $<
-check: a.out main
-	tr -cd a-z < /dev/urandom | head -c 1000 > /tmp/in
-	bash -c 'diff <(./main < /tmp/in) <(./a.out < /tmp/in)'
+CXXFLAGS := -g
+prog := fastBuildFastSearch fastBuildNaiveSearch naiveBuildFastSearch naiveBuildNaiveSearch
+
+define test
+	@(tr -cd a-d < /dev/urandom | head -c 1000; echo; tr -cd a-d < /dev/urandom | head -c 3) > /tmp/in
+	bash -c 'diff -B <(./$(@:test_%=%) < /tmp/in | xargs -n 1 | sort -n) <(./all-occurrences < /tmp/in)'
+
+	@(tr -cd a-c < /dev/urandom | head -c 2000; echo; tr -cd a-d < /dev/urandom | head -c 5) > /tmp/in
+	bash -c 'diff -B <(./$(@:test_%=%) < /tmp/in | xargs -n 1 | sort -n) <(./all-occurrences < /tmp/in)'
+endef
+
+define NL
+
+
+endef
+
+fastBuildFastSearch: main.cpp
+	$(CXX) $(CXXFLAGS) -D LINEAR_BUILD -D LINEAR_SEARCH $< -o $@
+
+fastBuildNaiveSearch: main.cpp
+	$(CXX) $(CXXFLAGS) -D LINEAR_BUILD $< -o $@
+
+naiveBuildFastSearch: main.cpp
+	$(CXX) $(CXXFLAGS) -D LINEAR_SEARCH $< -o $@
+
+naiveBuildNaiveSearch: main.cpp
+	$(CXX) $(CXXFLAGS) $< -o $@
+
+$(foreach i,$(prog),$(eval test_$i: $i$(NL)	$$(test)))
+
+test_all: $(foreach i,$(prog),test_$i)
--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <vector>
+#include <list>
 using namespace std;
 
 // #define LINEAR_BUILD
+// #define LINEAR_SEARCH
 
 class PositionHeap {
     char *T;
@@ -14,6 +15,22 @@ class PositionHeap {
         int childs[256];
         Node() { fill_n(childs, 256, -1); }
     } *nodes;
+#ifdef LINEAR_BUILD
+    int *parent;
+#endif
+#ifdef LINEAR_SEARCH
+    int *maxReach, *discovery, *finish;
+    void dfs(int pos, int &timestamp) {
+        if (pos == -1) return;
+        discovery[pos] = timestamp++;
+        for (int i = 0; i < 256; i++)
+            dfs(nodes[pos].childs[i], timestamp);
+        finish[pos] = timestamp++;
+    }
+    bool isDescendant(int v, int u) {
+        return discovery[u] <= discovery[v] && finish[v] <= finish[u];
+    }
+#endif
     void preorderPH(int pos, int depth) {
         if (pos == -1) return;
         printf("%*s%d children:", 2*depth, "", pos);
@@ -24,30 +41,27 @@ class PositionHeap {
         for (int i = 0; i < 256; i++)
             preorderPH(nodes[pos].childs[i], depth+1);
     }
-#ifdef LINEAR_SEARCH
-#else
-    void appendSubtree(int pos, vector<int> &ret) {
+    void appendSubtree(int pos, list<int> &ret) {
         if (pos == -1) return;
         ret.push_back(pos);
         for (int i = 0; i < 256; i++)
             appendSubtree(nodes[pos].childs[i], ret);
     }
-#endif
 public:
 #ifdef LINEAR_BUILD
     PositionHeap(const char T[]) {
         n = (int)strlen(T);
         this->T = strdup(T);
-        nodes = new Node[n];
-        int *parent = new int[n], *depth = new int[n];
+        nodes = new Node[n+1];
+        parent = new int[n];
+        int *depth = new int[n];
         
-        depth[n-1] = 0;
-        for (int i = n-2; i >= 0; i--) {
+        for (int i = n; --i >= 0; ) {
             const char *q = &T[i];
-            if (nodes[n-1].childs[(int)*q] == -1) {
-                parent[i] = n-1;
+            if (nodes[n].childs[(int)*q] == -1) {
+                parent[i] = n;
                 depth[i] = 1;
-                nodes[n-1].childs[(int)*q] = i;
+                nodes[n].childs[(int)*q] = i;
             } else {
                 int u = i+1, v, w;
                 do {
@@ -60,60 +74,136 @@ public:
                 nodes[v].childs[(int)*q] = i;
             }
         }
-        // installMaxReach();
+        
+    #ifdef LINEAR_SEARCH
+        maxReach = new int[n];
+        for (int u = n, i = n; --i >= 0; ) {
+            const char *q = &T[i];
+            int w;
+            while ((w = nodes[u].childs[(int)*q]) == -1)
+                u = parent[u];
+            maxReach[i] = w;
+            u = w;
+        }
+    #endif
 
         for (int i = n; --i >= 0; ) {
             new(&nodes[i]) Node;
-            if (i < n-1)
-                nodes[parent[i]].childs[(int)T[i+depth[i]-1]] = i;
+            nodes[parent[i]].childs[(int)T[i+depth[i]-1]] = i;
         }
         delete[] parent;
         delete[] depth;
+
+   #ifdef LINEAR_SEARCH
+        discovery = new int[n+1];
+        finish = new int[n+1];
+        int timestamp = 0;
+        dfs(n, timestamp);
+   #endif
     }
 #else
     PositionHeap(const char T[]) {
         n = (int)strlen(T);
         this->T = strdup(T);
-        nodes = new Node[n];
-        for (int i = n-2; i >= 0; i--) {
-            int v = n-1;
+        nodes = new Node[n+1];
+        for (int i = n; --i >= 0; ) {
             const char *q = &T[i];
+            int v = n;
             while (nodes[v].childs[(int)*q] != -1)
                 v = nodes[v].childs[(int)*q++];
             nodes[v].childs[(int)*q] = i;
         }
+    #ifdef LINEAR_SEARCH
+        maxReach = new int[n];
+        for (int i = n; --i >= 0; ) {
+            const char *q = &T[i];
+            int v = n;
+            while (nodes[v].childs[(int)*q] != -1)
+                v = nodes[v].childs[(int)*q++];
+            maxReach[i] = v;
+        }
+        discovery = new int[n+1];
+        finish = new int[n+1];
+        int timestamp = 0;
+        dfs(n, timestamp);
+    #endif
     }
 #endif
     void preorder() {
-        preorderPH(n-1, 0);
+        preorderPH(n, 0);
     }
     ~PositionHeap() {
         free(T);
         delete[] nodes;
+#ifdef LINEAR_SEARCH
+        delete[] discovery;
+        delete[] finish;
+#endif
     }
-    vector<int> search(const char S[]) {
-        vector<int> ret;
-        int m = (int)strlen(S), depth = 0, v = n-1;
-        while (v != -1 && *S != '\0') {
-            if (strncmp(&T[v], S+depth, m-depth) == 0)
+#ifdef LINEAR_SEARCH
+    list<int> search(const char S[]) {
+        list<int> ret;
+        const char *p = S;
+        int v = n;
+        while (*p && nodes[v].childs[(int)*p] != -1)
+            v = nodes[v].childs[(int)*p++];
+        p = S;
+        int u = n;
+        while (*p && nodes[u].childs[(int)*p] != -1) {
+            u = nodes[u].childs[(int)*p++];
+            if (isDescendant(maxReach[u], v))
+                ret.push_back(u);
+        }
+        if (!*p)
+            for (int i = 0; i < 256; i++)
+                appendSubtree(nodes[v].childs[i], ret);
+        while (*p) {
+            const char *q = p;
+            v = n;
+            while (*p && nodes[v].childs[(int)*p] != -1)
+                v = nodes[v].childs[(int)*p++];
+            if (p == q) {
+                ret.clear();
+                break;
+            }
+            for (list<int>::iterator it = ret.begin(); it != ret.end(); )
+                if (*it+(q-S) < n && isDescendant(maxReach[*it+(q-S)], v))
+                    ++it;
+                else
+                    it = ret.erase(it);
+        }
+        return ret;
+    }
+#else
+    list<int> search(const char S[]) {
+        list<int> ret;
+        int m = (int)strlen(S), depth = 0, v = n;
+        while (v != -1 && *S) {
+            if (strncmp(T+v+depth, S, m-depth) == 0)
                 ret.push_back(v);
             v = nodes[v].childs[(int)*S++];
+            depth++;
         }
         appendSubtree(v, ret);
         return ret;
     }
+#endif
 };
 
 int main()
 {
-    char T[3601], S[2217];
+    static char T[3612217], S[3612217];
+    
     scanf("%s", T);
     PositionHeap *heap = new PositionHeap(T);
-    heap->preorder();
+    // heap->preorder();
+    
+    scanf("%s", S);
+    list<int> positions = heap->search(S);
+    for (list<int>::const_iterator it = positions.begin(); it != positions.end(); ) {
+        printf("%d", *it);
+        putchar(++it == positions.end() ? '\n' : ' ');
+    }
+
     delete heap;
-    // scanf("%s", S);
-    // vector<int> positions = heap->search(S);
-    // for (vector<int>::const_iterator it = positions.begin(); it != positions.end(); it++)
-    //     printf("%d ", *it);
-    // putchar('\n');
 }
